cf2cadc529129c8d55c2807c30b7ad0b152641f5
Edivane Novais <103701219+EdivaneNovais@users.noreply.github.com>
2022-06-02 12:43:38 -0300

    corrigi exercicio Fibonacci

--- introducao_python/exercicio_fibonacci.py
+++ introducao_python/exercicio_fibonacci.py
@@ -1,14 +1,17 @@
 anterior = 0
 proximo = 1
 c = 3
-n = (int(input("Digite até que número você quer a sequência:")))
-print(f"({anterior}")
-print(f"{proximo}")
-
-while c <= n:
+n = (int(input("Digite até que número você quer a sequência Fibonacci:")))
+if n == 1:
+    print(f"{anterior}")
+elif n > 1:
+    print(f"{anterior}")
+    print(f"{proximo}")
+while c <= n: 
     aux = anterior + proximo
     print(f"{aux}")
+
     anterior = proximo
-    proximo = aux
-    c =  c + 1
+    proximo = aux 
+    c = c + 1
 print("FIM")
--- introducao_python/exercicio_fibonacci_for.py
+++ introducao_python/exercicio_fibonacci_for.py
@@ -1,12 +1,15 @@
-anterior = 0
+anterior = 0 
 proximo = 1
 c = 3
 n = int(input("Digite até que número você quer a sequência Fibonacci:"))
-print (anterior)
-print (proximo)
-for c in range (c, n+1):
+if n == 1:
+    print(anterior)
+elif n > 1:
+    print(anterior)
+    print(proximo)
+for c in range(c, n+1):
     aux = anterior + proximo
-    print (aux)
+    print(aux)
 
     anterior = proximo 
     proximo = aux
