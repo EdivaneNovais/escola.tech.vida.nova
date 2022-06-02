969800e69e01ac8805df89cb06aabf12c4a37bae
Edivane Novais <103701219+EdivaneNovais@users.noreply.github.com>
2022-06-02 15:34:36 -0300

    corregi o exercicio fibonacci com estrutura de repetição for e while

--- introducao_python/exercicio_fibonacci.py
+++ introducao_python/exercicio_fibonacci.py
@@ -1,17 +1,27 @@
-anterior = 0
-proximo = 1
-c = 3
 n = (int(input("Digite até que número você quer a sequência Fibonacci:")))
-if n == 1:
-    print(f"{anterior}")
-elif n > 1:
-    print(f"{anterior}")
-    print(f"{proximo}")
-while c <= n: 
-    aux = anterior + proximo
-    print(f"{aux}")
+sair = 1
+while sair != 0:
+    anterior = 0
+    proximo = 1
+    c = 3
+    if n == 1:
+        print(f"{anterior}")
+    elif n > 1:
+        print(f"{anterior}")
+        print(f"{proximo}")
+    while c <= n: 
+        aux = anterior + proximo
+        print(f"{aux}")
 
-    anterior = proximo
-    proximo = aux 
-    c = c + 1
-print("FIM")
+        anterior = proximo
+        proximo = aux 
+        c = c + 1
+    print("FIM\n")
+    
+    sair = (int(input("digite novamente a sequência que deseja de numeros Fibonacci, ou digite 0(zero) para encerrar:")))
+
+    if sair == 0:
+        break
+    n = sair 
+    
+print("\nOBRIGADO")
--- introducao_python/exercicio_fibonacci_for.py
+++ introducao_python/exercicio_fibonacci_for.py
@@ -1,17 +1,28 @@
-anterior = 0 
-proximo = 1
-c = 3
 n = int(input("Digite até que número você quer a sequência Fibonacci:"))
-if n == 1:
-    print(anterior)
-elif n > 1:
-    print(anterior)
-    print(proximo)
-for c in range(c, n+1):
-    aux = anterior + proximo
-    print(aux)
+sair = 1
+while sair != 0:
+    anterior = 0 
+    proximo = 1
+    c = 3
+    if n == 1:
+        print(anterior)
+    elif n > 1:
+        print(anterior)
+        print(proximo)
+    for c in range(c, n+1):
+        aux = anterior + proximo
+        print(aux)
 
-    anterior = proximo 
-    proximo = aux
-    c += 1
-print("FIM")
+        anterior = proximo 
+        proximo = aux
+        c += 1
+    print("FIM\n")
+
+    sair = (int(input("digite novamente a sequência que deseja de numeros Fibonacci, ou digite 0(zero) para encerrar:")))
+
+    if sair == 0:
+        break
+    n = sair 
+
+
+print("\nOBRIGADO")
