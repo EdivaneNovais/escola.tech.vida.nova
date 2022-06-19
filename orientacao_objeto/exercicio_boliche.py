b81853ee500a6d00d4de26faeaa8621fdf0ded0e
Edivane Novais <103701219+EdivaneNovais@users.noreply.github.com>
2022-06-19 17:26:48 -0300

    corrigi o while do exercicio boliche

--- orientacao_objeto/exercicio_boliche.py
+++ orientacao_objeto/exercicio_boliche.py
@@ -9,6 +9,7 @@ pista = [
     "I", " ", "I", " ", "I", " ", "I", "\n", " ", "I", " ", "I", " ", "I", "\n", " "," ", "I", " ", "I", "\n", " ", " ", " ", "I", " "
 ]
 
+
 numero_dos_pinos = {
     "1": 24,
     "2": 17,
@@ -22,18 +23,16 @@ numero_dos_pinos = {
     "10": 6
 }
 
-jogada = [random.randint(1,10)]
 x=0
-while x < (len(pista)): 
+while x < 10: 
     jogada = [random.randint(1, 10)]
     for pino in jogada:
         posicao = numero_dos_pinos[str(pino)]
-        pista[posicao] = "_"
-        for posicao in pista:
-            if posicao == "I" or posicao == "\n":
-                continue
-    if pista == "_":
-        break
+        if pista[posicao] == "_":
+            x = x - 1
+            print("Você não derrubou nada")
+        else:
+            pista[posicao] = "_"          
     x = x + 1
-  
+
     mostra_pista(pista) 
