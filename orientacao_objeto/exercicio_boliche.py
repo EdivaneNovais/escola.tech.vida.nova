89a13fe29e1912b5fc90e7f46248387d350ca2e5
Edivane Novais <103701219+EdivaneNovais@users.noreply.github.com>
2022-07-05 12:02:41 -0300

    inclui a funcao choices para sortear mais de um numero

--- orientacao_objeto/exercicio_boliche.py
+++ orientacao_objeto/exercicio_boliche.py
@@ -1,4 +1,5 @@
-import random
+from random import choices, randint
+
 
 def mostra_pista(pinos):
     for e in pinos:
@@ -20,19 +21,25 @@ numero_dos_pinos = {
     "7": 0,
     "8": 2,
     "9": 4,
-    "10": 6
-}
+    "10": 6 }
 
 x=0
-while x < 10: 
-    jogada = [random.randint(1, 10)]
+while x < 10:
+    sorteio = randint(1, 4) #defini a quantidade de numeros que serão sorteados
+    jogada = choices([1,2,3,4,5,6,7,8,9,10], k = sorteio)
+        
     for pino in jogada:
+        pinos_derrubados = 0
         posicao = numero_dos_pinos[str(pino)]
         if pista[posicao] == "_":
-            x = x - 1
-            print("Você não derrubou nada")
+            print("Você não derrubou")
         else:
-            pista[posicao] = "_"          
-    x = x + 1
+            pista[posicao] = "_"  
+            print('Você acertou o pino, parabéns') 
+             
+            x = x + 1
 
     mostra_pista(pista) 
+
+print("Você derrubou todos os pinos")
+  
