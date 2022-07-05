04b85eb94d5927ca654fa73c0506d0fdabc2f652
Edivane Novais <103701219+EdivaneNovais@users.noreply.github.com>
2022-07-05 11:40:09 -0300

    alterei a funçao que calcula o troco

--- orientacao_objeto/exercicio_quitanda.py
+++ orientacao_objeto/exercicio_quitanda.py
@@ -1,3 +1,19 @@
+def calculo_troco (divida, valor_pago):
+        troco = valor_pago - divida
+        if divida == valor_pago :
+            print("o dinheiro a pagar é igual ao dinheiro recebido")
+        if valor_pago < divida:
+            print("ERRO!!!\nO valor recebido é inferior ao valor a pagar")
+        else:
+            troco = valor_pago - divida
+            print(f"o valor do troco é de R$ {(troco)}0")
+
+            for cedula in [20, 5, 1]:
+                quantidade = troco // cedula
+                troco = troco % cedula
+                if quantidade > 0:
+                    print(f"{int(quantidade)} nota(s) de R$ {cedula},00")
+                    
 precos = {
     'morango': 7,
     'melancia' : 20,
@@ -16,7 +32,7 @@ resposta = 's'
 n = False
 while resposta != "n":
     print(mensagem)
-    fruta = input('que fruta voĉe gostaria? ').lower()
+    fruta = input('que fruta voĉe gostaria?(Escreva o nome da fruta):').lower()
     if fruta in precos:
         preco = precos[fruta]
         print(f'o preco unitario dessa fruta é: R${preco},00')
@@ -31,20 +47,7 @@ while resposta != "n":
     
     if resposta == 'n':
         dinheirorecebido = float(input("dinheiro recebido:"))
-        def calculo_troco (divida, valor_pago):
-            troco = valor_pago - divida
-            if total == dinheirorecebido :
-                print("o dinheiro a pagar é igual ao dinheiro recebido")
-            if dinheirorecebido < total:
-                print("ERRO!!!\nO valor recebido é inferior ao valor a pagar")
-            else:
-                troco = dinheirorecebido - total
-                print(f"o valor do troco é de {(troco)}0")
-
-                for cedula in [20, 5, 1]:
-                    quantidade = troco // cedula
-                    troco = troco % cedula
-                    if quantidade > 0:
-                        print(f"{int(quantidade)} nota(s) de R$ {cedula},00")
     
         calculo = calculo_troco(total,dinheirorecebido)
+
+print("\nObrigado, Volte sempre!")
