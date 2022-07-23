b56fb835b0c0bd3b273088a5c0fa13a28dc443ed
Edivane Novais <103701219+EdivaneNovais@users.noreply.github.com>
2022-07-23 11:22:33 -0300

    corrigi as funções do exercicio arquivo

--- orientacao_objeto/exercicio_arquivo/exercicio_brincando_palavras.py
+++ orientacao_objeto/exercicio_arquivo/exercicio_brincando_palavras.py
@@ -58,7 +58,7 @@ def total_letras_iniciais():
     'o':0, 'p':0, 'q':0, 'r':0, 's':0, 't':0, 'u':0, 'v':0, 'w':0, 'x':0, 'y':0, 'z':0}
 
     for palavra in lista_texto:
-        primeira_letra = str(palavra[0])
+        primeira_letra = (palavra[0])
         if primeira_letra == 'a':
             alfabetos[primeira_letra] += 1
         elif primeira_letra == 'b':
